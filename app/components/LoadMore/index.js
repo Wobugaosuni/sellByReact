6b21570b3fc61a05aa08087b254b7f97e539d781
qiuxia <397401374@qq.com>
2017-06-10 21:50:37 +0800

    首页自动加载更多

--- app/components/LoadMore/index.js
+++ app/components/LoadMore/index.js
@@ -12,14 +12,39 @@ export default class LoadMore extends React.Component {
 
   render() {
     return (
-      <div role="component:LoadMore">
+      <div role="component:LoadMore" ref="loadMore">
         {
           this.props.isLoadingMore ?
           <span>加载中...</span> :
-          <span className="load-more" onClick={this.onLoadingMoreClick.bind(this)}>加载更多</span>
+          <span
+            className="load-more"
+            onClick={this.onLoadingMoreClick.bind(this)}
+          >
+            加载更多
+          </span>
         }
       </div>
     );
+  }
+
+  componentDidMount() {
+    const loadMoreElement = this.refs.loadMore;
+    let This = this;
+    // console.log('loadMoreElement', loadMoreElement);
+
+    // 监听下拉动作
+    addEventListener('scroll', () => {
+
+      // 获取"加载更多"相对于视图顶部的位置
+      let loadMoreElementTop = loadMoreElement.getBoundingClientRect().top;
+
+      let windowHeight = window.screen.height;
+
+      if (loadMoreElementTop < windowHeight) {
+        This.props.loadMoreData();
+      }
+
+    }, false);
   }
 
   onLoadingMoreClick() {
